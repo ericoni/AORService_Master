462def57c265a06066abbfdd1df2f747f77c8f21
Dragan <ericoni94@gmail.com>
2019-03-20 22:06:53 +0100

    Added test console and EventAlarmService

---
+++ EventAlarmService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAlarmService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
