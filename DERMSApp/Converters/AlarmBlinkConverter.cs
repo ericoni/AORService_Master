8dcbad519e4ddcfc5ca895e65f350ef16f68b391
Dragan Eric <ericoni94@gmail.com>
2019-08-06 15:20:13 +0200

    Alarm blinking. Severity foreground tbd (in events ).

---
+++ DERMSApp/Converters/AlarmBlinkConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DERMSApp.Converters
+{
+	public class AlarmBlinkConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			//if ((int)value > 0)
+			//{
+			//	return FetchStyle("FaderStyleButton");
+			//}
+			//return null;
+
+			return FetchStyle("FaderStyleButton");
+
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		Style FetchStyle(string name)
+		{
+			return Application.Current.Resources[name] as Style;
+		}
+	}
+}
